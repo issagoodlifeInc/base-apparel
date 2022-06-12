dd3e2895b977ce86ff83c42aaa12afe0c9f01b25
Lesley <leskimuti@gmail.com>
2022-06-12 04:21:38 +0300

    Third commit - Fixed accessibility issues from Frontend Mentor page

--- main.js
+++ main.js
@@ -2,16 +2,15 @@ const submit = document.querySelector('#submit');
 const email = document.querySelector('#email');
 let message = document.querySelector('.form__msg-err');
 const iconErr = document.querySelector('.form__icon-err');
-
+let value = email.value;
 
 submit.addEventListener('click', function(e) {
-  let value = email.value;
   if (value.length == 0 || value == '') {
     e.preventDefault();
   }
 
   if (validateEmail(value)) {
-    message.textContent = `${value} is a valid email`;
+    message.textContent = `${email.value} is a valid email`;
     message.style.color = "green";
     message.style.visibility = "visible";
     iconErr.style.visibility = "hidden";
@@ -20,10 +19,17 @@ submit.addEventListener('click', function(e) {
     iconErr.style.visibility = "visible";
     message.style.visibility = "visible";
   }
+
 });
 
 
-function validateEmail(email) {
-  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+function validateEmail(value) {
+  // const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  // return re.test(String(email).toLowerCase());
+  //
+  // /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+  // /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+
+  return (/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email.value))
 }
